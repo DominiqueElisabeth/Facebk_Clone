9793407b0b8c88dfd11fd96cdf0232d0843d41cd
dominique owoua <domelisa@ymail.com>
2021-06-04 08:07:20 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :posts
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create, :show]
+  resources  :users
+
+  resources :posts do
+    collection do
+    post :confirm
+  end
+  end
+  resources :favorites, only: [:create, :destroy]
 end
