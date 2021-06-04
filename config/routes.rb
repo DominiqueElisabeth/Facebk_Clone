c7dfbbb6536642657928dbcd0623c9c52acdc368
dominique owoua <domelisa@ymail.com>
2021-06-04 04:23:08 +0000

    session creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :posts
-  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
+  resources :users, only: [:new, :create, :show]
 end
