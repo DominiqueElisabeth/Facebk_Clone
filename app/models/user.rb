2f84794c5e349ba617c5a46bb1dbdde2abaa1dc2
dominique owoua <domelisa@ymail.com>
2021-06-04 13:37:43 +0000

    update

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ApplicationRecord
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
     before_validation { email.downcase! }
-    attr_accessor :password, :password_confirmation
+    has_secure_password
     validates :password, presence: true, length: { minimum: 6 }
 end
